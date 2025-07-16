8dd26bb4e4f8267a897b0e40337159afc86414f5
cd-z <p_loser_austria@gmx.at>
2025-07-16 10:01:31 +0200

    copy theme for pnpm

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: "class",
   important: true,
